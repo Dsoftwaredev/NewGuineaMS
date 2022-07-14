91ee48b253d855cb307e34f42bed995d60dd9e7f
naym-r <naym_r@hotmail.co.uk>
2022-07-14 14:34:59 +0100

    Manager class updated

--- src/main/java/com/qa/newguinea/main/persistance/Manager.java
+++ src/main/java/com/qa/newguinea/main/persistance/Manager.java
@@ -1,5 +1,24 @@
 package com.qa.newguinea.main.persistance;
 
 public class Manager {
-
+	private int managerid;
+	private String managername;
+	public Manager(int managerid, String managername) {
+		super();
+		this.managerid = managerid;
+		this.managername = managername;
+	}
+	public int getManagerid() {
+		return managerid;
+	}
+	public void setManagerid(int managerid) {
+		this.managerid = managerid;
+	}
+	public String getManagername() {
+		return managername;
+	}
+	public void setManagername(String managername) {
+		this.managername = managername;
+	}
+	
 }
