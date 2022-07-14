2503c82745f81c712336d8a364a9da4df66bde76
naym-r <naym_r@hotmail.co.uk>
2022-07-14 15:57:44 +0100

    added delivery controller

---
+++ src/main/java/com/qa/newguinea/main/controllers/DeliveryController.java
@@ -0,0 +1,45 @@
+package com.qa.newguinea.main.controllers;
+
+import java.util.List;
+
+import com.qa.newguinea.main.persistance.Delivery;
+
+public class DeliveryController implements CrudController<Delivery> {
+
+	@Override
+	public List<Delivery> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Delivery read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Delivery readLatest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Delivery create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Delivery update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
