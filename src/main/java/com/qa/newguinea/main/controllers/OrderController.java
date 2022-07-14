4af62a9249172d34de5dd262a62d09b8e747685a
naym-r <naym_r@hotmail.co.uk>
2022-07-14 15:55:32 +0100

    added order controller

--- src/main/java/com/qa/newguinea/main/controllers/OrderController.java
+++ src/main/java/com/qa/newguinea/main/controllers/OrderController.java
@@ -1,5 +1,45 @@
 package com.qa.newguinea.main.controllers;
 
-public class OrderController {
+import java.util.List;
+
+import com.qa.newguinea.main.persistance.Order;
+
+public class OrderController implements CrudController<Order> {
+
+	@Override
+	public List<Order> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order readLatest() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Order update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
