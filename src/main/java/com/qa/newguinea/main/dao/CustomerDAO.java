60d58888ee38f6390444176f29b3b1479885a39d
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-07-15 11:20:13 +0100

    made CustomerDAO.java made, unfinished

--- src/main/java/com/qa/newguinea/main/dao/CustomerDAO.java
+++ src/main/java/com/qa/newguinea/main/dao/CustomerDAO.java
@@ -1,20 +1,71 @@
 package com.qa.newguinea.main.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import com.qa.newguinea.main.persistance.Customers;
+import com.qa.newguinea.utils.DBUtils;
 
 public class CustomerDAO implements DAO<Customers> {
 
 	@Override
 	public Customers read(int id) {
-		// TODO Auto-generated method stub
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers WHERE customer_id = ?;");) {
+			statement.setInt(1, id);
+			try (ResultSet resultSet = statement.executeQuery();) {
+				resultSet.next();
+				return modelFromResult(resultSet);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 		return null;
 	}
-
+	
+	public Customers readByUserId(int id) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers WHERE user_id = ?;");) {
+			statement.setInt(1, id);
+			try (ResultSet resultSet = statement.executeQuery();) {
+				resultSet.next();
+				return modelFromResult(resultSet);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return null;
+	}
+	
+	public Customers readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("SELECT * FROM customers WHERE ORDER BY customer_id DESC LIMIT 1;");) {
+			try (ResultSet resultSet = statement.executeQuery();) {
+				resultSet.next();
+				return modelFromResult(resultSet);
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return null;
+	}
+	
 	@Override
-	public Customers create(Customers t) {
-		// TODO Auto-generated method stub
+	public Customers create(Customers c) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO customers(customer_name, user_id, customer_address) VALUES (?, ?, ?);");) {
+			statement.setString(1, c.getCustName());
+			statement.setInt(2, c.getUserID());
+			statement.setString(3, c.getCustAdress());
+			statement.executeUpdate();
+			return readLatest();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+		return null;
 		return null;
 	}
 
